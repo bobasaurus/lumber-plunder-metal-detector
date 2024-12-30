e6164402ad164e72e09813f3c87049e936999c05
Allen (home laptop) <allen.jordan@gmail.com>
2024-12-30 14:39:44 -0700

    adding a vs project to calculate the sine wave resolutions required to avoid missing dac steps

---
+++ visual studio/sine_wave_resolution_test/sine_wave_resolution_test/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace sine_wave_resolution_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.numUniquePointsLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.zedGraphControl1.Location = new System.Drawing.Point(12, 12);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(938, 520);
+            this.zedGraphControl1.TabIndex = 0;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // numUniquePointsLabel
+            // 
+            this.numUniquePointsLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.numUniquePointsLabel.AutoSize = true;
+            this.numUniquePointsLabel.Location = new System.Drawing.Point(16, 515);
+            this.numUniquePointsLabel.Name = "numUniquePointsLabel";
+            this.numUniquePointsLabel.Size = new System.Drawing.Size(35, 13);
+            this.numUniquePointsLabel.TabIndex = 1;
+            this.numUniquePointsLabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(962, 544);
+            this.Controls.Add(this.numUniquePointsLabel);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Label numUniquePointsLabel;
+    }
+}
+
---
+++ visual studio/sine_wave_resolution_test/sine_wave_resolution_test/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sine_wave_resolution_test
+{
+    public partial class Form1 : Form
+    {
+        private const UInt32 WAVE_BUFFER_CAPACITY = 50000;
+
+        UInt16 waveAmplitude;//DAC counts ranging from [0,4095]
+        UInt16 waveFrequency;//[Hz]
+        UInt32 waveSize;//[points]
+        UInt16[] waveBuffer = new UInt16[WAVE_BUFFER_CAPACITY];
+
+        void GenerateWaveToTransmit(double sampleRate, double frequency, double amplitude, double center)
+        {
+            double sampleTime = 1.0 / sampleRate;//[sec/sample]
+            UInt32 samplesPerCycle = (UInt32)Math.Round(sampleRate / frequency);
+
+            if (samplesPerCycle >= WAVE_BUFFER_CAPACITY) throw new Exception("wave buffer too small");
+
+            for (UInt32 i = 0; i < samplesPerCycle; i++)
+            {
+                double timeS = (i) * sampleTime;
+                double waveValue = amplitude * Math.Sin(2 * Math.PI * frequency * timeS) + center;
+                UInt16 waveValueInt = (UInt16)Math.Round(waveValue);//TODO: try just truncating instead of rounding
+                if (waveValueInt < 0) throw new Exception("wave value too small");
+                if (waveValueInt > 4095) throw new Exception("wave value too large");
+                waveBuffer[i] = waveValueInt;
+            }
+
+            waveAmplitude = (UInt16)amplitude;
+            waveFrequency = (UInt16)frequency;
+            waveSize = samplesPerCycle;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+
+                double sampleRate = 10_000_000;//samples/sec
+                double frequency = 10000;
+                double amplitude = 2047.5;
+                double center = 2047.5;
+                GenerateWaveToTransmit(sampleRate, frequency, amplitude, center);
+
+                //count the number of unique points in the first 1/4 cycle, which should equal the amplitude if no dac points are skipped due to a low sampling rate
+                HashSet<double> hashSet = new HashSet<double>();
+                for (int i = 0; i < waveSize / 4; i++) hashSet.Add(waveBuffer[i]);
+                numUniquePointsLabel.Text = "num unique points in first 1/4 cycle: " + hashSet.Count;
+                if ((hashSet.Count) < Math.Round(amplitude, 0)) MessageBox.Show("Missing Points!! (" + (hashSet.Count) + "/" + amplitude + ")");
+
+
+                var pointPairList = new ZedGraph.PointPairList();
+                for (int i = 0; i < waveSize; i++) pointPairList.Add(i, waveBuffer[i]);
+                var curve = zedGraphControl1.GraphPane.AddCurve("sine wave", pointPairList, Color.Green);
+                curve.Symbol.Type = ZedGraph.SymbolType.Circle;
+                curve.Symbol.Fill = new ZedGraph.Fill(curve.Color);
+                zedGraphControl1.GraphPane.XAxis.Scale.MaxGrace = 0;
+                zedGraphControl1.GraphPane.YAxis.Scale.MaxGrace = 0;
+                zedGraphControl1.GraphPane.XAxis.Scale.MinGrace = 0;
+                zedGraphControl1.GraphPane.YAxis.Scale.MinGrace = 0;
+                zedGraphControl1.GraphPane.Title.IsVisible = false;
+                zedGraphControl1.GraphPane.Legend.IsVisible = false;
+                zedGraphControl1.AxisChange();
+                zedGraphControl1.Refresh();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
